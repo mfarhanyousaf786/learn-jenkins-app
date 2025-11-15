2118fdb319e5e01e5f4692d8ffa66ad05736d102
mfarhanyousaf786 <mfarhanyousaf786@gmail.com>
2025-11-15 21:13:30 +0000

    Test

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -1,13 +1,12 @@
 // @ts-check
-//const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-/*test('has title', async ({ page }) => {
+test('has title', async ({ page }) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Learn Jenkins/);
 });
-
 
 test('has Jenkins in the body', async ({ page }) => {
   await page.goto('/');
@@ -25,4 +24,4 @@ test('has expected app version', async ({ page }) => {
 
   const isVisible = await page.locator(`p:has-text("Application version: ${expectedAppVersion}")`).isVisible();
   expect(isVisible).toBeTruthy();
-});*/
+});
